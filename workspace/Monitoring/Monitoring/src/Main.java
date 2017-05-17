e474c7fe31ee8f0d2e12506ff5d6c865f075f7a7
Timkie91 <s4tikies@uni-trier.de>
2017-05-17 13:58:15 +0200

    Update Main.java

--- workspace/Monitoring/Monitoring/src/Main.java
+++ workspace/Monitoring/Monitoring/src/Main.java
@@ -29,7 +29,7 @@ import org.xml.sax.helpers.XMLReaderFactory;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-public class Maain {
+public class Main {
 
 	public static void main(String[] args) {
 		try {
